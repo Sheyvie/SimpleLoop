84b1d3e6a042913e40d9a9716391e73b8f9cdd0c
Sheyvie <sheilayvi@gmail.com>
2023-08-21 09:49:20 +0300

    first commit

---
+++ SimpleLoop/Program.cs
@@ -0,0 +1,17 @@
+﻿
+//create a program that prints the first 'n' even numbers where 'n' is taken from the user
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(" Enter the value of n: ");
+
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        for (int i =2; i<=2*n; i += 2)
+        {
+            Console.WriteLine(i+ " ");
+        }
+    }
+}
